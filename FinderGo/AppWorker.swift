ca21294b6d2293d14979df91d123f7b0dd8cb72c
Qianli Ma <mqlzdfz@gmail.com>
2022-08-08 14:53:15 +0800

    Fixed not working on M1 mac issue

--- FinderGo/AppWorker.swift
+++ FinderGo/AppWorker.swift
@@ -11,13 +11,38 @@ import Cocoa
 struct AppWorker {
 
   func finderCurrentPath() -> String? {
-    guard let scriptUrl = Bundle.main.url(forResource: "finderCurrentPath", withExtension: "scpt"),
-      let string = try? String(contentsOf: scriptUrl) else {
-        return nil
-    }
-
-    let script = NSAppleScript(source: string)
-    return script?.executeAndReturnError(nil).stringValue
+      guard let scriptUrl = Bundle.main.url(forResource: "finderCurrentPath", withExtension: "scpt"),
+            let script = try? String(contentsOf: scriptUrl) else {
+          return nil
+          }
+//      let scriptURL = Bundle.main.url(forResource: "finderCurrentPath", withExtension: "scpt")
+//      let script = try? String(contentsOf: scriptURL) else {
+//                  return nil
+      var error: NSDictionary?
+      if let scriptObject = NSAppleScript(source: script)
+      {
+          let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(&error)
+//          print(output.stringValue as Any)
+//          let alert = NSAlert()
+          
+          if (error != nil)
+          {
+//              var _: NSError?
+//              guard let errorJson = try? JSONSerialization.data(withJSONObject:error as Any, options: JSONSerialization.WritingOptions.prettyPrinted) else { return default nil }
+//              let json = NSString(data:errorJson, encoding:String.Encoding.utf8.rawValue)
+//              let errorString :String = json! as String
+//              alert.messageText = errorString
+//              alert.messageText = "ERRRRRR"
+//              alert.runModal()
+              return nil
+          }
+//          alert.messageText = output.stringValue!
+//          alert.runModal()
+          return output.stringValue
+      }
+      return nil
+//    let script = NSAppleScript(source: string)
+//    return script?.executeAndReturnError(nil).stringValue
   }
 
   func findTerminal() -> String {
